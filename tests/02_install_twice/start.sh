3184ced58608694f462bf3203fbaa1865392d9ef
shai atias <shai@experitest.com>
2019-04-10 15:13:24 +0300

    ADD 3 tests and split by folder

---
+++ tests/02_install_twice/start.sh
@@ -0,0 +1,30 @@
+
+echo "[STEP 1] installing dependencies"
+
+ansible-galaxy install git+https://github.com/ExperitestOfficial/ansible-role-app-signer.git,$TRAVIS_BRANCH
+
+echo "[STEP 1] installing dependencies - completed successfully"
+
+echo "[STEP 2] starting ansible playbook"
+
+ansible-playbook site.yml
+
+echo "[STEP 2] starting ansible playbook - completed successfully"
+
+echo "[STEP 3] verifying install"
+
+echo "skipping verify"
+
+echo "[STEP 3] verifying install - completed successfully"
+
+echo "[STEP 4] reinstalling"
+
+ansible-playbook site.yml
+
+echo "[STEP 4] reinstalling - completed successfully"
+
+echo "[STEP 5] verifying install"
+
+echo "skipping verify"
+
+echo "[STEP 5] verifying install - completed successfully"
---
+++ tests/03_install_uninstall/start.sh
@@ -0,0 +1,30 @@
+
+echo "[STEP 1] installing dependencies"
+
+ansible-galaxy install git+https://github.com/ExperitestOfficial/ansible-role-app-signer.git,$TRAVIS_BRANCH
+
+echo "[STEP 1] installing dependencies - completed successfully"
+
+echo "[STEP 2] starting ansible playbook"
+
+ansible-playbook install.yml
+
+echo "[STEP 2] starting ansible playbook - completed successfully"
+
+echo "[STEP 3] verifying install"
+
+echo "skipping verify"
+
+echo "[STEP 3] verifying install - completed successfully"
+
+echo "[STEP 4] uninstall"
+
+ansible-playbook uninstall.yml
+
+echo "[STEP 4] uninstall - completed successfully"
+
+echo "[STEP 5] verifying uninstall"
+
+echo "skipping verify"
+
+echo "[STEP 5] verifying uninstall - completed successfully"
